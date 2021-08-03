d146ecdf7a52f1ac5bc55404bf3d432f4c3db533
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2021-08-03 13:51:57 -0300

    feat: improve database configuration file

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,4 @@
-export const database = require('knex')({
-  client: 'mysql',
-  connection: {
-    host: '127.0.0.1',
-    user: 'root',
-    password: '123',
-    database: 'graphql_example',
-  },
-})
+const environment = process.env.ENVIRONMENT || 'development'
+const config = require('../../knexfile.js')[environment]
+
+export const database = require('knex')(config)
